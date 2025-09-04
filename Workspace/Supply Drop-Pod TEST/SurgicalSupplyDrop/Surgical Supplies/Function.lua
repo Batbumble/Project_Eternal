48613a64f057ece8078db9cf07721ce2c81f9818
Batbumble <69385900+Batbumble@users.noreply.github.com>
2025-09-04 04:35:00 +0100

    Delete Workspace/Supply Drop-Pod TEST directory

--- Workspace/Supply Drop-Pod TEST/SurgicalSupplyDrop/Surgical Supplies/Function.lua
+++
@@ -1,8 +0,0 @@
--- @ScriptType: Script
-script.Parent.ProximityPrompt.Triggered:Connect(function(player)
-
-game:GetService("ServerStorage"):WaitForChild(script.Parent.Name):Clone().Parent = player.Backpack
-script.Parent.PickUp:Destroy()
-script.Parent:Destroy()
-
-end)

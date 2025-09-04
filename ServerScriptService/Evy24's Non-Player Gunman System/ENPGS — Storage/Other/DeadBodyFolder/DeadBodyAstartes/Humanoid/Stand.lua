e55f10b710f04a85a578fb3bbd3e6e69485c44e5
Batbumble <69385900+Batbumble@users.noreply.github.com>
2025-09-04 08:20:48 +0100

    Delete ServerScriptService directory

--- ServerScriptService/Evy24's Non-Player Gunman System/ENPGS — Storage/Other/DeadBodyFolder/DeadBodyAstartes/Humanoid/Stand.lua
+++
@@ -1,4 +0,0 @@
--- @ScriptType: Script
-wait(.1)
-script.Parent.Sit = true
-script.Parent.PlatformStand = true
--- ServerScriptService/Evy24's Non-Player Gunman System/ENPGS — Storage/Other/Special/Thunderhawk UM2/Model/DoorOpen/TempScript.lua
+++
@@ -1,7 +0,0 @@
--- @ScriptType: Script
-task.wait(20)
-local weld = Instance.new("WeldConstraint",script.Parent)
-weld.Name = "TempWeld"
-weld.Part0 = script.Parent.Parent.PrimaryPart
-weld.Part1 = script.Parent
-script:Destroy()
